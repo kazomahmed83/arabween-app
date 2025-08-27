7407efef47a394f7ee5c3401546afb083b860b42
Mayank-siddhiinfosoft <mayank@siddhiinfosoft.com>
2025-08-27 19:04:20 +0530

    updated

--- lib/app/auth_screen/singup_screen.dart
+++ lib/app/auth_screen/singup_screen.dart
@@ -1,4 +1,3 @@
-import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
